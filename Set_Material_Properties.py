38fa960e4694ba30dd16441b8893a24a5fd98aeb
Lofty <73583091+CCrashCup@users.noreply.github.com>
2023-11-20 15:21:23 -0500

    Add files via upload

--- Set_Material_Properties.py
+++ Set_Material_Properties.py
@@ -11,25 +11,33 @@ if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode='OBJECT')
 
 # Properties to set must be True otherwise False
-BlendMode   = True
+AlphaValue  = True
+EmitPower   = True
 BackFace    = True
+BlendMode   = True
 
 #
 ## Be sure to set the intended value(s) below, that you want for any properties flagged True above.
 #
 
-#blend_mode = 'OPAQUE'
-#blend_mode = 'CLIP'
-#blend_mode = 'HASHED'
-blend_mode  = 'BLEND'
-blend_mode_count = 0
+alpha_value = 1.0
+alpha_value_count  = 0
 
-#back_face   = False
-back_face   = True
+emit_power  = 1.0
+emit_power_count  = 0
+
+back_face   = False
+#back_face   = True
 back_face_count = 0
 
+blend_mode = 'OPAQUE'
+#blend_mode = 'CLIP'
+#blend_mode = 'HASHED'
+#blend_mode  = 'BLEND'
+blend_mode_count = 0
+
 mat_list    = []
-print("*****************************************************************************")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 for obj in bpy.context.selected_objects:
     if obj.type == 'MESH':
         for slot in obj.material_slots:
@@ -38,16 +46,27 @@ for obj in bpy.context.selected_objects:
                 mat = slot.material
                 nodes = mat.node_tree.nodes
                 nodeB = nodes.get("Principled BSDF")
+                if AlphaValue:
+                    if not nodeB.inputs['Alpha'].links:
+                        nodeB.inputs['Alpha'].default_value = alpha_value
+                        alpha_value_count += 1
+                if EmitPower:
+                    nodeB.inputs['Emission Strength'].default_value = emit_power
+                    emit_power_count += 1
+                if BackFace:
+                    mat.use_backface_culling = back_face
+                    back_face_count += 1
                 if BlendMode:
-                    if nodeB.inputs['Alpha'].links:
+                    if nodeB.inputs['Alpha'].links or (not nodeB.inputs['Alpha'].links and AlphaValue):
                         mat.blend_method = blend_mode
                         blend_mode_count += 1
-                if BackFace:
-                        mat.use_backface_culling = back_face
-                        back_face_count += 1
 
 print("*****************************************************************************")
-if BlendMode:
-    print(f"{blend_mode_count} materials had Alpha Blend Mode set to {blend_mode}.")
+if AlphaValue:
+    print(f"{alpha_value_count} materials had Emission Strength set to {alpha_value}.")
+if BackFace:
+    print(f"{emit_power_count} materials had Emission Strength set to {emit_power}.")
 if BackFace:
     print(f"{back_face_count} materials had BackFace Culling set to {back_face}.")
+if BlendMode:
+    print(f"{blend_mode_count} materials had Alpha Blend Mode set to {blend_mode}.")
