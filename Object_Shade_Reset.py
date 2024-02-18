f7cdaf54ca10d8c04bc85e9f625516e60cad48b5
Lofty <73583091+CCrashCup@users.noreply.github.com>
2024-02-18 09:56:59 -0500

    Bug fix.

--- Object_Shade_Reset.py
+++ Object_Shade_Reset.py
@@ -11,7 +11,7 @@ import bpy
 import math
 from math import *
 
-ASangle = 30        # AutoSmooth angle to use.
+ASangle = 45        # AutoSmooth angle to use.
 AutoSmooth = True   # If True, do AutoSmooth. If False, check Smooth.
 
 Smooth = True       # If True and AutoSmooth is False, do Smooth. If both are False, do Flat.
@@ -21,18 +21,20 @@ Scount = 0
 if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
+if AutoSmooth:
+    bpy.ops.object.shade_smooth(use_auto_smooth = True)
+elif Smooth:
+    bpy.ops.object.shade_smooth(use_auto_smooth = False)
+else:
+    bpy.ops.object.shade_smooth(use_auto_smooth = False)
+    bpy.ops.object.shade_flat()
+
 for obj in bpy.context.selected_objects:
     if obj.type == 'MESH':
         bpy.context.view_layer.objects.active = obj
         bpy.ops.mesh.customdata_custom_splitnormals_clear()
         if AutoSmooth:
             bpy.context.object.data.auto_smooth_angle = radians(ASangle)
-            bpy.ops.object.shade_smooth(use_auto_smooth = True)
-        elif Smooth:
-            bpy.ops.object.shade_smooth(use_auto_smooth = False)
-        else:
-            bpy.ops.object.shade_smooth(use_auto_smooth = False)
-            bpy.ops.object.shade_flat()
         Scount += 1
 
 print(f"Objects processed  = {Scount}")
