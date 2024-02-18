074444e0ae601ef3500bce12796d90d9460c8e01
Lofty <73583091+CCrashCup@users.noreply.github.com>
2024-02-18 17:52:07 -0500

    Reworking to make more flexible.

--- Normals_Clean_Up.py
+++ Normals_Clean_Up.py
@@ -3,24 +3,29 @@
 #   Coded by Lofty from the code of "marbs" in a StackExchange post referred by @flyingsaucer.
 #   You may select the entire mesh, or a partial area, that contains errant face normals.
 #   This will hopefully detect the errant normals and then, optionally, flip them for you.
-#   If you set the variable "auto_flip" to False then the object will remain in Edit Mode
+#   If you set the variable "auto_Flip" to False then the object will remain in Edit Mode
 #   with the errant faces selected.
+#   If you set the variable "do_Snap" to False then the reference will be World Origin
+#   instead of Object Origin.
 #   Be sure to check the box for Face Orientation in the Overlays menu so you can watch.  
+#   It can be complex, where multiple passes are needed, with different options active.
 #
 import bpy
 import bmesh
 import mathutils
 
-auto_flip = True
+auto_Flip = True        # Flip the calculated faces (otherwise just leave them selected).
+do_Snap = True          # Snap the Cursor to the current object's origin.
+
+#allow = 0.0001         # Greater than zero
+allow = 0               # This value can be adjusted to get more or fewer faces that are angled.
+#allow = -0.0001        # Less than zero
 
 print("=======================================")
 
-allow = 0
-# Set to object mode.
 if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
-# Get the active object
 obj = bpy.context.active_object
 
 faces=[]
@@ -28,24 +33,25 @@ if obj and obj.type == 'MESH':
     mesh = bpy.context.active_object
     # Get selected faces
     selected_faces = [f.index for f in mesh.data.polygons if f.select]
-    
     # Loop through the selected faces.
     for face_index in selected_faces:
         face = mesh.data.polygons[face_index]
         normal = face.normal
-#       Original code below. Replaced.
+        if do_Snap:
+            view_location = mathutils.Vector((0.0, 0.0, 0.0))       # Force view_location to Object Origin
+        else:
         # Get location of the current 3d view.
-#        area = next(area for area in bpy.context.window.screen.areas if area.type == 'VIEW_3D')
-#        view_location = area.spaces.active.region_3d.view_matrix.inverted().translation
-        view_location = mathutils.Vector((0.0, 0.0, 0.0))       # Force view_location to Object Origin
-        # modify the view location so that it is at (0,0,0) and calculate 
-        # the dot product with the normal.
+            area = next(area for area in bpy.context.window.screen.areas if area.type == 'VIEW_3D')
+            view_location = area.spaces.active.region_3d.view_matrix.inverted().translation
+        # Calculate the dot product with the normal.
         dot_product = normal.dot(view_location - face.center)
-#       Original code below. Replaced.
-        # If the dot product is negative then this is a 'red' face.
-#        if dot_product < allow:
-        if dot_product >= allow:
-            faces.append(face_index)
+        if do_Snap:
+            if dot_product >= allow:
+                faces.append(face_index)
+        else:
+            if dot_product <= allow:
+                faces.append(face_index)
+
                 
     # Deselect all faces
     bpy.ops.object.mode_set(mode = 'EDIT')
@@ -56,15 +62,17 @@ if obj and obj.type == 'MESH':
     for face in faces:
         obj.data.polygons[face].select = True
 
-    bpy.context.area.ui_type = 'VIEW_3D'        # Required for Snap to work
-    bpy.ops.view3d.snap_cursor_to_active()
-    bpy.context.area.ui_type = 'TEXT_EDITOR'    # Reset back to normal
+    if do_Snap:
+        bpy.context.area.ui_type = 'VIEW_3D'        # Required for Snap to work
+        bpy.ops.view3d.snap_cursor_to_active()
+        bpy.context.area.ui_type = 'TEXT_EDITOR'    # Reset back to normal
     bpy.ops.object.mode_set(mode = 'EDIT')
-    if auto_flip:
+    if auto_Flip:
         bpy.ops.mesh.flip_normals()
         bpy.ops.object.mode_set(mode = 'OBJECT')
-    bpy.context.area.ui_type = 'VIEW_3D'        # Required for Snap to work
-    bpy.ops.view3d.snap_cursor_to_center()
-    bpy.context.area.ui_type = 'TEXT_EDITOR'    # Reset back to normal
+    if do_Snap:
+        bpy.context.area.ui_type = 'VIEW_3D'        # Required for Snap to work
+        bpy.ops.view3d.snap_cursor_to_center()
+        bpy.context.area.ui_type = 'TEXT_EDITOR'    # Reset back to normal
 else:
     print("No mesh object currently selected.")
