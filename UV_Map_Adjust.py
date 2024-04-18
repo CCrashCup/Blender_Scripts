e88c286a278015441e0f71079177eac9283eee2a
Lofty <73583091+CCrashCup@users.noreply.github.com>
2024-04-18 07:12:18 -0400

    Added Y axis mirroring. Arma 3 scale.

--- UV_Map_Adjust.py
+++ UV_Map_Adjust.py
@@ -11,7 +11,10 @@ import bpy
 
 #scale = 0.0004875           # Armored Core 6
 #scale = 0.000975            # Gundam Breaker Mobile
-scale = 0.00024375          # War Thunder's CDK
+#scale = 0.00024375          # War Thunder's CDK
+#FLIP = False
+scale = 0.0039               # Arma 3 from NR 1.7.1 rip / needs FLIP
+FLIP = True
 
 if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode='OBJECT')
@@ -27,6 +30,8 @@ for obj in bpy.context.visible_objects:
         bpy.ops.object.mode_set(mode = 'EDIT')
         bpy.ops.uv.select_all(action='SELECT')
         bpy.ops.transform.resize(value=(scale, scale, 1.0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, use_accurate=True)
+        if FLIP:
+            bpy.ops.transform.mirror(orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False))
         bpy.ops.uv.snap_selected(target='CURSOR_OFFSET')
         bpy.ops.transform.translate(value=(0.5, 0.5, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
         bpy.ops.object.editmode_toggle()
