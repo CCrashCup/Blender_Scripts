e20d5a34c1525b77d5cbd7f39e624c247cdf5f59
Lofty <73583091+CCrashCup@users.noreply.github.com>
2025-04-08 18:22:47 -0400

    Handle duplicates with Blender numeric suffixes.

--- Image_Extension.py
+++ Image_Extension.py
@@ -4,20 +4,20 @@
 #   This is a short script to walk through every object in a scene
 #   and change all image texture file name extensions. The initial
 #   purpose is for pointing at files converted from .DDS to another
-#   format like .PNG. The script does does not care what the source
-#   file extension is.
-#   Change the target extension "ext" to fit your image file type.
+#   format like .PNG.
+#   Change the source extension "from_ext" to fit your old image file type.
+#   Change the target extension "to_ext" to fit your new image file type.
 #   The "name" and the "filepath" are both changed to avoid confusion.
 #   The "name" is for Blender internal reference only and does not
 #   change any external file names.
-#   The script will work even if you have not yet converted the files.
-#   You will then need to "Find Missing Files" after the conversion.
+#   The script will work even if you have not yet converted the files,
+#   but you will then need to "Find Missing Files" after the conversion.
 #
 import os
 import bpy
 
-ext = ".png"
-#ext = ".dds"
+from_ext = ".dds"
+to_ext = ".png"
 
 if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode='OBJECT')
@@ -27,12 +27,15 @@ count = 0
 for image in bpy.data.images:
     opath = bpy.data.images[count].filepath
     oname = bpy.data.images[count].name
+    if not (os.path.splitext(oname)[1] == from_ext):
+        oname = os.path.splitext(oname)[0]
     if opath > "":
-        npath = os.path.splitext(opath)[0] + ext
-        nname = os.path.splitext(oname)[0] + ext
-        bpy.data.images[count].filepath = npath
-        bpy.data.images[count].name = nname
-        count += 1
+        if os.path.splitext(oname)[1] == from_ext:
+            npath = os.path.splitext(opath)[0] + to_ext
+            nname = os.path.splitext(oname)[0] + to_ext
+            bpy.data.images[count].filepath = npath
+            bpy.data.images[count].name = nname
+    count += 1
 
 print("*****************************************************************************")
 print(f"{count} image names processed.")
