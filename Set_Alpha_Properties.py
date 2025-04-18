9a72c92b61e6d988a71d54d1ee781422db27ca52
Lofty <73583091+CCrashCup@users.noreply.github.com>
2025-04-18 18:02:41 -0400

    Added the option for Alpha Mode in the Image Texture nodes.

--- Set_Alpha_Properties.py
+++ Set_Alpha_Properties.py
@@ -1,8 +1,8 @@
 # Set Alpha Properties
 #
 #   Coded by Lofty
-#   This is a short script to walk through the materials for
-#   every selected object in a scene and change various properties.
+#   This is a short script to walk through the materials for every selected
+#   object in a scene and change various properties related to Alpha.
 #   Be sure to adjust all settings before running.
 #
 import bpy
@@ -10,25 +10,36 @@ import bpy
 if bpy.ops.object.mode_set.poll():
     bpy.ops.object.mode_set(mode='OBJECT')
 
-# Properties to set must be True otherwise False
-AlphaConnect  = False
-AlphaValue    = False
-BlendMode     = False
-#
-## Be sure to set the intended value(s) below, that you want for any properties flagged True above.
-#
+# Properties to set must be True - otherwise False
+AlphaConnect     = False
+AlphaDisConnect  = False
+AlphaValue       = False
+AlphaMode        = False
+BlendMode        = False
 
-alpha_connect_count  = 0
-
+#
+## Be sure to set the intended value(s) below, that you want for any properties flagged "True" above.
+#
 alpha_value = 1.0
-alpha_value_count  = 0
+
+#alpha_mode  = 'STRAIGHT'
+#alpha_mode  = 'PREMUL'
+alpha_mode  = 'CHANNEL_PACKED'
+#alpha_mode  = 'NONE'
 
 #blend_mode = 'OPAQUE'
 #blend_mode = 'CLIP'
 #blend_mode = 'HASHED'
 blend_mode  = 'BLEND'
-blend_mode_count = 0
 
+#
+## Do not change items below here.
+#
+alpha_connect_count  = 0
+alpha_disconnect_count  = 0
+alpha_value_count  = 0
+alpha_mode_count = 0
+blend_mode_count = 0
 
 mat_list    = []
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
@@ -46,10 +57,22 @@ for obj in bpy.context.selected_objects:
                         nodeT = nodeB.inputs['Base Color'].links[0].from_node
                         links.new(nodeT.outputs['Alpha'],nodeB.inputs['Alpha'])
                         alpha_connect_count += 1
+                if AlphaDisConnect:
+                    if nodeB.inputs['Alpha'].is_linked:
+                        links.remove(nodeB.inputs['Alpha'].links[0])
+                        nodeB.inputs['Alpha'].default_value = 1.0
+                        mat.blend_method = 'OPAQUE'
+                        alpha_disconnect_count += 1
                 if AlphaValue:
                     if not nodeB.inputs['Alpha'].is_linked:
                         nodeB.inputs['Alpha'].default_value = alpha_value
                         alpha_value_count += 1
+                if AlphaMode:
+                    for node in nodes:
+                        if node.type == 'TEX_IMAGE' and node.image:
+                            if node.image.has_data:
+                                node.image.alpha_mode = alpha_mode
+                                alpha_mode_count += 1
                 if BlendMode:
                     if nodeB.inputs['Alpha'].is_linked or (not nodeB.inputs['Alpha'].is_linked and AlphaValue):
                         mat.blend_method = blend_mode
@@ -58,7 +81,11 @@ for obj in bpy.context.selected_objects:
 print("*****************************************************************************")
 if AlphaConnect:
     print(f"{alpha_connect_count} materials had Alpha connected.")
+if AlphaDisConnect:
+    print(f"{alpha_disconnect_count} materials had Alpha disconnected.")
 if AlphaValue:
     print(f"{alpha_value_count} materials had Alpha set to {alpha_value}.")
+if AlphaMode:
+    print(f"{alpha_mode_count} material textures had Alpha Mode set to {alpha_mode}.")
 if BlendMode:
     print(f"{blend_mode_count} materials had Alpha Blend Mode set to {blend_mode}.")
