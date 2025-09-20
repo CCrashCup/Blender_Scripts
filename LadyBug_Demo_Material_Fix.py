1d796885aa972720b68811843c42982bd25a7f97
Lofty <73583091+CCrashCup@users.noreply.github.com>
2025-09-19 23:05:47 -0400

    Update LadyBug_Demo_Material_Fix.py
    Catch possible errors so it doesn't crash.

--- LadyBug_Demo_Material_Fix.py
+++ LadyBug_Demo_Material_Fix.py
@@ -12,9 +12,9 @@
 #     Note: Here is the layout template of the Image Texture Nodes.
 #
 #                               ┌──────┐
-#                        3   0  │ BSDF │
-#                        4   1  │      │
-#                        5   2  │      │
+#                9   6   3   0  │ BSDF │
+#               10   7   4   1  │      │
+#               11   8   5   2  │      │
 #                               └──────┘
 
 import bpy
@@ -37,62 +37,69 @@ for obj in bpy.context.selected_objects:
     if obj.type == 'MESH':
         for slot in obj.material_slots:
             if slot.material not in mat_list:
-                mat_list.append(slot.material)
                 mat = slot.material
-                nodeBP = mat.node_tree.nodes.get("Principled BSDF")
+                mat_list.append(slot.material)
+                try:
+                    nodeBP = mat.node_tree.nodes.get("Principled BSDF")
+                except:
+                    continue
                 nodeBP.location = (nodeBP.location.x - 10), (nodeBP.location.y)
 
                 countT = 0
+                nodeTC = nodeTC = nodeTN = None
                 for node in mat.node_tree.nodes:
                     if node.type == 'TEX_IMAGE':
                         if node.name[-4:] == f'{position_T1/1000}'[-4:]:
                             nodeTC = node
                         elif node.name[-4:] == f'{position_T3/1000}'[-4:]:
-                            nodeTR = node
+                            nodeTC = node
                         elif node.name[-4:] == f'{position_T4/1000}'[-4:]:
                             nodeTN = node
                         else:           # Collect extra nodes out of the way
                             node.location = (nodeBP.location.x + (300 + (countT * 50))), (nodeBP.location.y - (300 + (countT * 50)))
                             countT += 1
 #
-                nodeTC.location = (nodeBP.location.x - 1500), (nodeBP.location.y - 85)
-                mat.node_tree.links.new(nodeTC.outputs['Color'], nodeBP.inputs['Base Color'])
+                if nodeTC:
+                    nodeTC.location = (nodeBP.location.x - 1500), (nodeBP.location.y - 85)
+                    mat.node_tree.links.new(nodeTC.outputs['Color'], nodeBP.inputs['Base Color'])
 
-                nodeTRI = mat.node_tree.nodes.new("ShaderNodeInvert")
-                nodeTRI.location = (nodeBP.location.x - 300), (nodeBP.location.y - 299)
-                mat.node_tree.links.new(nodeTRI.outputs['Color'], nodeBP.inputs['Roughness'])
+                    if nodeTR:
+                        nodeTRI = mat.node_tree.nodes.new("ShaderNodeInvert")
+                        nodeTRI.location = (nodeBP.location.x - 300), (nodeBP.location.y - 299)
+                        mat.node_tree.links.new(nodeTRI.outputs['Color'], nodeBP.inputs['Roughness'])
 
-                nodeTR.image.colorspace_settings.name = 'Non-Color'
-                nodeTR.image.alpha_mode = 'CHANNEL_PACKED'
-                nodeTR.location = (nodeBP.location.x - 900), (nodeBP.location.y - 350)
-                mat.node_tree.links.new(nodeTR.outputs['Color'], nodeTRI.inputs['Color'])
-                #
-                ## Baby Poop Normal Map Conversion
-                #
-                nodeNM = mat.node_tree.nodes.new("ShaderNodeNormalMap")
-                nodeNM.location = (nodeBP.location.x - 300), (nodeBP.location.y - 603)
-                mat.node_tree.links.new(nodeNM.outputs['Normal'], nodeBP.inputs['Normal'])
+                        nodeTR.image.colorspace_settings.name = 'Non-Color'
+                        nodeTR.image.alpha_mode = 'CHANNEL_PACKED'
+                        nodeTR.location = (nodeBP.location.x - 900), (nodeBP.location.y - 350)
+                        mat.node_tree.links.new(nodeTR.outputs['Color'], nodeTRI.inputs['Color'])
+                    #
+                    ## Baby Poop Normal Map Conversion
+                    #
+                    if nodeTN:
+                        nodeNM = mat.node_tree.nodes.new("ShaderNodeNormalMap")
+                        nodeNM.location = (nodeBP.location.x - 300), (nodeBP.location.y - 603)
+                        mat.node_tree.links.new(nodeNM.outputs['Normal'], nodeBP.inputs['Normal'])
 
-                nodeNCC = mat.node_tree.nodes.new("ShaderNodeCombineColor")
-                nodeNCC.location = (nodeBP.location.x - 600), (nodeNM.location.y)
-                mat.node_tree.links.new(nodeNCC.outputs['Color'], nodeNM.inputs['Color'])
+                        nodeNCC = mat.node_tree.nodes.new("ShaderNodeCombineColor")
+                        nodeNCC.location = (nodeBP.location.x - 600), (nodeNM.location.y)
+                        mat.node_tree.links.new(nodeNCC.outputs['Color'], nodeNM.inputs['Color'])
 
-                nodeNSCBI = mat.node_tree.nodes.new("ShaderNodeInvert")
-                nodeNSCBI.location = (nodeBP.location.x - 900), (nodeBP.location.y - 745)
-                mat.node_tree.links.new(nodeNSCBI.outputs['Color'], nodeNCC.inputs['Blue'])
+                        nodeNSCBI = mat.node_tree.nodes.new("ShaderNodeInvert")
+                        nodeNSCBI.location = (nodeBP.location.x - 900), (nodeBP.location.y - 745)
+                        mat.node_tree.links.new(nodeNSCBI.outputs['Color'], nodeNCC.inputs['Blue'])
 
-                nodeNSC = mat.node_tree.nodes.new("ShaderNodeSeparateColor")
-                nodeNSC.location = (nodeBP.location.x - 1200), (nodeBP.location.y - 659)
-                mat.node_tree.links.new(nodeNSC.outputs['Red'], nodeNCC.inputs['Red'])
-                mat.node_tree.links.new(nodeNSC.outputs['Green'], nodeNCC.inputs['Green'])
-                mat.node_tree.links.new(nodeNSC.outputs['Blue'], nodeNSCBI.inputs['Color'])
+                        nodeNSC = mat.node_tree.nodes.new("ShaderNodeSeparateColor")
+                        nodeNSC.location = (nodeBP.location.x - 1200), (nodeBP.location.y - 659)
+                        mat.node_tree.links.new(nodeNSC.outputs['Red'], nodeNCC.inputs['Red'])
+                        mat.node_tree.links.new(nodeNSC.outputs['Green'], nodeNCC.inputs['Green'])
+                        mat.node_tree.links.new(nodeNSC.outputs['Blue'], nodeNSCBI.inputs['Color'])
 
-                nodeTN.image.colorspace_settings.name = 'Non-Color'
-                nodeTN.image.alpha_mode = 'CHANNEL_PACKED'
-                nodeTN.location = (nodeBP.location.x - 1500), (nodeNM.location.y)
-                mat.node_tree.links.new(nodeTN.outputs['Color'], nodeNSC.inputs['Color'])
+                        nodeTN.image.colorspace_settings.name = 'Non-Color'
+                        nodeTN.image.alpha_mode = 'CHANNEL_PACKED'
+                        nodeTN.location = (nodeBP.location.x - 1500), (nodeNM.location.y)
+                        mat.node_tree.links.new(nodeTN.outputs['Color'], nodeNSC.inputs['Color'])
 
-                count += 1
+                    count += 1
 
 print("*****************************************************************************")
 print(f"{count} texture node groups relinked")
