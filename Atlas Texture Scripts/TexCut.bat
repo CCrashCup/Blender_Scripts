e77e1c3e0580797f65023ea5d068a2ce0e7c0f54
Lofty <73583091+CCrashCup@users.noreply.github.com>
2026-02-05 12:09:32 -0500

    Added the BAT files.
    For those that wish to do it all for themselves if they don't like downloading uncertified executables.

---
+++ Atlas Texture Scripts/TexCut.bat
@@ -0,0 +1,10 @@
+REM Arguments are:
+REM 	1 = Input Filetype
+REM 	2 = List of Comma Separated Row Dividing Line Locations
+REM 	3 = List of Comma Separated Column Dividing Line Locations
+REM The output will be created in a new folder named "New".
+REM
+REM Example: TexCut png "512,1024" "512,1024,1536,2048"
+REM
+TexCut.exe png "512,1024" "512,1024,1536,2048"
+pause
---
+++ Atlas Texture Scripts/TexSplit.bat
@@ -0,0 +1,10 @@
+REM Arguments are:
+REM 	1 = Input Filetype
+REM 	2 = Number of Image Rows
+REM 	3 = Number of Image Columns
+REM The output will be created in a new folder named "New".
+REM
+REM Example: TexSplit png 2 4
+REM
+TexSplit.exe png 2 4
+pause
